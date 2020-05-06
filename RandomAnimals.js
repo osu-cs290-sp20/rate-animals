240ed9c7ecb40e56bbdcb58a4e76cc363c577499
Jacob Eckroth <eckrothj@oregonstate.edu>
2020-05-05 21:38:03 -0700

    fixed collision with button

--- RandomAnimals.js
+++ RandomAnimals.js
@@ -1,12 +1,13 @@
 
 
 
-//run every 1 second.
-var intervalBetweenAnimals = window.setInterval(generateAnimal,2000);
+//To change how oftten animals appear, just change the number in the sentInterval. It represents milliseconds.
+var intervalBetweenAnimals = window.setInterval(generateAnimal,1000);
 
 let buttonHolder = document.getElementById("button-holder");
 
 let images=buttonHolder.getElementsByClassName("animal-image");;
+let button = document.getElementById("rate-button")
 
 function generateAnimal(){
     
@@ -14,7 +15,7 @@ function generateAnimal(){
     
     newImage.classList.add("animal-image")
     let randomHeight = (100 +(Math.random() * 200))
-    newImage.style.height=randomHeight+"px"; //we can make this a random number later.
+    newImage.style.height=randomHeight+"px";
     newImage.src="http://placekitten.com/"+Math.floor(randomHeight)+"/"+Math.floor(randomHeight); //this will be what we change.
     newImage.style.width="auto";
     newImage.style.boxShadow="10px 10px 10px #0000004d";
@@ -22,15 +23,20 @@ function generateAnimal(){
     newImage.style.transition = "opacity 1s, top 1s"
     newImage.style.opacity = "0";
     newImage.style.zIndex = "-1";
-    let objectheight = Math.random() * (buttonHolder.clientHeight-randomHeight); //eventually we can change the size and change the height.
-    let objectwidth = Math.random() * (buttonHolder.clientWidth-randomHeight);
-    //minus 300 because we need to make sure it's in the box not at the edges.
-    newImage.style.left = objectwidth+"px";
-    newImage.style.top = objectheight+"px";
-    appearImage(objectheight,newImage);
+    let objectHeight;
+    let objectWidth;
+    do{
+        console.log("attempting place");
+        objectHeight = Math.random() * (buttonHolder.clientHeight-randomHeight); //eventually we can change the size and change the height.
+        objectWidth = Math.random() * (buttonHolder.clientWidth-randomHeight);
+        //minus 300 because we need to make sure it's in the box not at the edges.
+        newImage.style.left = objectWidth+"px";
+        newImage.style.top = objectHeight+"px";
+    }while(!collideWithButton(newImage));
+    appearImage(objectHeight,newImage);
 
     images = buttonHolder.getElementsByClassName("animal-image");
-    if(buttonHolder.childElementCount >= 11){
+    if(buttonHolder.childElementCount >= 15){
      
         dissapearImage();
     }
@@ -38,19 +44,100 @@ function generateAnimal(){
 }
 
 
+//Note that this function works by magic and I programmed it entirely without googling anything, which albeit was very dumb.
+function collideWithButton(newImage){ //DONT TOUCH - Jacob
+    let buttonHeight = button.clientHeight;
+    let buttonWidth = button.clientWidth;
+    let screenWidth = buttonHolder.clientWidth;
+    let screenHeight = buttonHolder.clientHeight;
+    let imageTop = newImage.style.top;
+    let imageLeft = newImage.style.left;
+    let imageHeight = newImage.style.height;
+    let imageWidth = imageHeight;
+    imageTop = Number(imageTop.substring(0,imageTop.length-2));
+    imageLeft = Number(imageLeft.substring(0,imageLeft.length-2));
+    imageWidth = Number(imageWidth.substring(0,imageWidth.length-2));
+    imageHeight = Number(imageHeight.substring(0,imageHeight.length-2));
+
+    
+
+    let imageBottom = imageTop + imageHeight;
+    let imageRight = imageLeft + imageWidth;
+    let midWidth = imageLeft + imageWidth/2;
+    let midHeight = imageTop + imageHeight/2;
+   
+        //if this happens we know that it's in the right y position
+    if(screenHeight/2 -buttonHeight/2 <= imageTop && screenHeight/2 +buttonHeight/2 >=imageTop){ //checking top left corner of image
+        //testing if it's inside the button.
+        if(screenWidth/2 - buttonWidth/2 <= imageLeft && screenWidth/2 +buttonWidth/2 >=imageLeft){
+            return false;
+        }
+    }
+    if(screenHeight/2 -buttonHeight/2 <= imageBottom && screenHeight/2 +buttonHeight/2 >=imageBottom){ //bottom left
+        //testing if it's inside the button.
+        if(screenWidth/2 - buttonWidth/2 <= imageLeft && screenWidth/2 +buttonWidth/2 >=imageLeft){
+            return false;
+        }
+    }
+    if(screenHeight/2 -buttonHeight/2 <= imageBottom && screenHeight/2 + buttonHeight/2 >=imageBottom){ //bottom right
+        //testing if it's inside the button.
+        if(screenWidth/2 - buttonWidth/2 <= imageRight && screenWidth/2 + buttonWidth/2 >=imageRight){
+            return false;
+        }
+    }
+    if(screenHeight/2 -buttonHeight/2 <= imageTop && screenHeight/2 +buttonHeight/2 >=imageTop){ //top right
+        //testing if it's inside the button.
+        if(screenWidth/2 - buttonWidth/2 <= imageRight && screenWidth/2 +buttonWidth/2 >=imageRight){
+            return false;
+        }
+    }
+
+    //Checking midpoints of edges
+    if(screenHeight/2 -buttonHeight/2 <= midHeight && screenHeight/2 +buttonHeight/2 >=midHeight){  //leftMid
+        //testing if it's inside the button.
+        if(screenWidth/2 - buttonWidth/2 <= imageLeft && screenWidth/2 +buttonWidth/2 >=imageLeft){
+            return false;
+        }
+    }
+    if(screenHeight/2 -buttonHeight/2 <= midHeight && screenHeight/2 +buttonHeight/2 >=midHeight){ //rightMid
+        //testing if it's inside the button.
+        if(screenWidth/2 - buttonWidth/2 <= imageRight && screenWidth/2 +buttonWidth/2 >=imageRight){
+            return false;
+        }
+    }
+    if(screenHeight/2 -buttonHeight/2 <= imageBottom && screenHeight/2 + buttonHeight/2 >=imageBottom){ //bottomMid
+        //testing if it's inside the button.
+        if(screenWidth/2 - buttonWidth/2 <= midWidth && screenWidth/2 + buttonWidth/2 >=midWidth){
+            return false;
+        }
+    }
+    if(screenHeight/2 -buttonHeight/2 <= imageTop && screenHeight/2 +buttonHeight/2 >=imageTop){ //topMid
+        //testing if it's inside the button.
+        if(screenWidth/2 - buttonWidth/2 <= midWidth && screenWidth/2 +buttonWidth/2 >=midWidth){
+            return false;
+        }
+    }
+    return true;
+
+
+}
+
+
 function appearImage(initialY,newImage){
- 
+    var initialOffset = 40;
     buttonHolder.appendChild(newImage);
-    newImage.style.top = initialY+40+"px";
-    if(images[images.length-2].style.opacity ==="0"){
-        images[images.length-2].style.opacity = "1";
-        let temptop = images[images.length-2].style.top;
-        temptop = temptop.substring(0,temptop.length-2);
-        temptop = Number(temptop);
-        temptop -=40;
-        images[images.length-2].style.top = temptop+"px";
+    newImage.style.top = initialY+initialOffset+"px";
+    if(images.length >1){
+        if(images[images.length-2].style.opacity ==="0"){
+            images[images.length-2].style.opacity = "1";
+            let temptop = images[images.length-2].style.top;
+            temptop = temptop.substring(0,temptop.length-2);
+            temptop = Number(temptop);
+            temptop -=initialOffset;
+            images[images.length-2].style.top = temptop+"px";
+            
         
-     
+        }
     }
 
 }
