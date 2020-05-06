a4fff8b37f67ab1001523633c88b256982cd33c0
Jacob Eckroth <eckrothj@oregonstate.edu>
2020-05-06 15:08:08 -0700

    made it round to one decimal place when you change the score.

--- rating/changeScoreTwoAnimals.js
+++ rating/changeScoreTwoAnimals.js
@@ -9,11 +9,11 @@ module.exports = function(firstID,firstScore,secondID,secondScore,winner){
     var probabilitySecond = (1.0/(1.0+Math.pow(10,(firstScore-secondScore)/400))); //probability of winning of player 2
     var probabilityFirst = (1.0/(1.0+Math.pow(10,(secondScore-firstScore)/400))); //probablility of winning of player 1
     if(winner===1){
-        firstScore = (firstScore + k*(1-probabilityFirst)).fixed(1);
-        secondScore = (secondScore + k*(0-probabilitySecond)).fixed(1);
+        firstScore = (firstScore + k*(1-probabilityFirst)).toFixed(1);
+        secondScore = (secondScore + k*(0-probabilitySecond)).toFixed(1);
     }else{
-        firstScore = (firstScore + k*(0-probabilityFirst)).fixed(1);
-        secondScore = (secondScore + k*(1-probabilitySecond)).fixed(1);
+        firstScore = (firstScore + k*(0-probabilityFirst)).toFixed(1);
+        secondScore = (secondScore + k*(1-probabilitySecond)).toFixed(1);
     }
     console.log("New score 1:"+firstScore);
     console.log("New score 2:"+secondScore);
