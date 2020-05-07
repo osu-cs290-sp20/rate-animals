580ea6e23bd5431d2e9ace8aa9ad3d78d9ab6c64
Jacob Eckroth <eckrothj@oregonstate.edu>
2020-05-07 16:14:11 -0700

    added basic rate container

--- buttonPress.js
+++ buttonPress.js
@@ -27,9 +27,43 @@ function buttonClicked(){ //might want to change this dissapearing thing.
     rateButton.style.transition ="box-shadow 1s, opacity 2s";
     rateButton.style.opacity = "0";
     rateButton.style.boxShadow="50px 50px 10px #0000004d"
-    setTimeout(() => {
-       //pull up the rating animals page here. 
-    }, 1000);
-    
-    
+    setTimeout(ratingScreen, 1000);
+}
+
+function ratingScreen(){
+    buttonHolder.removeChild(rateButton);
+    setTimeout(function(){
+        document.body.removeChild(buttonHolder)
+        addRateBox();
+    },100);
+}
+
+function addRateBox(){
+    let rateContainer = document.createElement("div");
+    let navbar = document.getElementById("topbar")
+    let navbarHeight = navbar.offsetHeight;
+    rateContainer.id = "rate-container";
+    rateContainer.style.width = "auto";
+    rateContainer.style.textAlign = "center";
+    rateContainer.style.display = "relative";
+    console.log("Navbar Height:" + navbarHeight);
+    rateContainer.style.height = window.innerHeight-navbarHeight+"px"; //this fills up the whole window except the navbar.
+
+    let rateBox = document.createElement("div");
+    rateContainer.appendChild(rateBox);
+    rateBox.style.backgroundColor = "white";
+    rateBox.style.width = "750px";
+    rateBox.style.height = "500px";
+    rateBox.style.borderRadius = "15px";
+    rateBox.style.display = "inline-block";
+    rateBox.style.boxShadow = "10px 10px 10px #0000004d"
+    rateBox.style.opacity = "0";
+    rateBox.style.transition = "margin 2s, opacity 2s, display 2s";
+    rateBox.style.margin = "300px"; //add stuff within this rate box to add animal pictures and such.
+    document.body.appendChild(rateContainer);
+    setTimeout(function(){
+        rateBox.style.margin = "100px"
+        rateBox.style.opacity = "1";
+    },200);
+
 }
