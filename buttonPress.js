d5f19f9f2f1db31c62374905353bf673c1ac210e
Jacob Eckroth <eckrothj@oregonstate.edu>
2020-05-07 11:45:53 -0700

    changed button press a lil bit

--- buttonPress.js
+++ buttonPress.js
@@ -24,7 +24,9 @@ function stopFillingButton(){
 function buttonClicked(){ //might want to change this dissapearing thing.
     fadeAllAnimals(); //called from RandomAnimals.js
     console.log("button clicked");
+    rateButton.style.transition ="box-shadow 1s, opacity 2s";
     rateButton.style.opacity = "0";
+    rateButton.style.boxShadow="50px 50px 10px #0000004d"
     setTimeout(() => {
        //pull up the rating animals page here. 
     }, 1000);
