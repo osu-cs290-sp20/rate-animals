b55b1c51460b1095d11ac233911cc83455243865
Jacob Eckroth <eckrothj@oregonstate.edu>
2020-05-06 21:17:24 -0700

    added max and min scores to ELO

--- rating/changeScoreTwoAnimals.js
+++ rating/changeScoreTwoAnimals.js
@@ -2,6 +2,9 @@
 
 //some pseudocode for the moment because the animal database isn't actually implemented yet.
 //if winner - 1, then animal one wins, else animal 2 wins.
+
+var minimumRank = 100;
+var maximumRank = 1600;
 module.exports = function(firstID,firstScore,secondID,secondScore,winner){
     console.log("changeScoreTwoAnimals");
     
@@ -15,7 +18,16 @@ module.exports = function(firstID,firstScore,secondID,secondScore,winner){
         firstScore = (firstScore + k*(0-probabilityFirst)).toFixed(1);
         secondScore = (secondScore + k*(1-probabilitySecond)).toFixed(1);
     }
-    console.log("New score 1:"+firstScore);
-    console.log("New score 2:"+secondScore);
-
+    if (firstScore < minimumRank){
+        firstScore = minimumRank;
+    }
+    if(firstScore > maximumRank){
+        firstScore = maximumRank;
+    }
+    if(secondScore < minimumRank){
+        secondScore = minimumRank;
+    }
+    if(secondScore > maximumRank){
+        secondScore = maximumRank;
+    }
 };
