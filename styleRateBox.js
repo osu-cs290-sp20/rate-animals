b86e1678ce515c7c5b310b4c3e437e3d107f93f6
Jacob Eckroth <eckrothj@oregonstate.edu>
2020-05-09 15:46:42 -0700

    fixed some basic styling, still not quite responsive text-wise

--- styleRateBox.js
+++ styleRateBox.js
@@ -15,63 +15,59 @@ function styleBox(){
     stuffHolder.style.display = "relative";
 
     stuffHolder.style.display = "flex";
-    stuffHolder.style.flexFlow = "row wrap";
+    stuffHolder.style.flexFlow = "column wrap";
     
-    let image1_holder = document.createElement("div");
-    let image2_holder = document.createElement("div");
-    let divider = document.createElement("div");
-    divider.style.width = "20%";
-    divider.style.height = "100%";
-   
-
-    let orDiv = document.createElement("p");
-    orDiv.textContent = "or";
-    
-    divider.appendChild(orDiv);
-
-
-    image1_holder.classList.toggle("image_holder");
-    image2_holder.classList.toggle("image_holder");
-    let image_holders = [image1_holder,image2_holder];
-    
-    let names = ["George","Rachel"];
-
+    let image_holder = document.createElement("div");
+    let text_holder = document.createElement("div");
+    text_holder.style.display = "flex";
+    text_holder.style.flexFlow = "row wrap";
     let name1 = document.createElement("p");
     let name2 = document.createElement("p");
-    name1.textContent = names[0];                                           //CHANGE THIS LATER SO W CAN HAVE DYNAMIC NAMES
-    name2.textContent = names[1];
-    name1.style.fontSize = "30px";
-    name2.style.fontSize = "30px";                                  //FIGURE OUT HOW TO MAKE THIS RESPONSIVE LATER
-    for(var i = 0; i < image_holders.length; i++){
-        
-        image_holders[i].style.display = "flex";
-        image_holders[i].style.flexFlow = "column wrap";
-        image_holders[i].style.height = "100%";
-        image_holders[i].style.width = "40%";
-        image_holders[i].style.alignItems = "center";
-        image_holders[i].style.justifyContent = "center";
-        
+    let name_div = document.createElement("p");
+    name_div.textContent = "or"
+    name1.textContent = "George"
+    name2.textContent = "Freddy"
+    name1.style.height = "auto";
+    name1.style.width = "40%";
 
-    }
+    name_div.style.width = "20%";
+    name_div.style.height = "100%";
 
+    name2.style.height = "auto";
+    name2.style.width = "40%";
+
+
+
+    text_holder.appendChild(name1);
+    text_holder.appendChild(name_div);
+    text_holder.appendChild(name2);
+  
+   image_holder.style.display = "flex";
+   image_holder.style.flexfolow = "row wrap";
+   image_holder.style.width = "100%";
+   image_holder.style.height = "60%";
 
     let image1 = document.createElement("img");
     let image2 = document.createElement("img");
+    let img_div = document.createElement("div");
+
+    img_div.style.width = "20%";
+    img_div.style.height = "100%";
+
     image1.src = "https://placekitten.com/200/200";                                 //CHANGE THIS TO HAVE DIFFERENT PICTURES
     image2.src = "https://placekitten.com/300/300";
     image1.style.height = "auto";
-    image1.style.width = "100%";
+    image1.style.width = "40%";
     image2.style.height = "auto";
-    image2.style.width = "100%";
+    image2.style.width = "40%";
 
-    image1_holder.appendChild(image1);
-    image2_holder.appendChild(image2);
-    image1_holder.appendChild(name1);
-    image2_holder.appendChild(name2);
 
-    stuffHolder.appendChild(image1_holder);
-    stuffHolder.appendChild(divider);
-    stuffHolder.appendChild(image2_holder);
+    stuffHolder.appendChild(image_holder);
+    stuffHolder.appendChild(text_holder);
+    image_holder.appendChild(image1);
+    image_holder.appendChild(img_div);
+    image_holder.appendChild(image2);
+
     rateBox.appendChild(stuffHolder);
 
 
