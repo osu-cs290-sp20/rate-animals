b10d2122288672a44fe63ba24ae656b4556c3b2b
jessgarcia <60108571+cia14@users.noreply.github.com>
2020-06-11 02:57:31 -0700

    update leaderboard display for each animal

--- public/leaderboards/leaderboardTemplate.js
+++ public/leaderboards/leaderboardTemplate.js
@@ -8,14 +8,14 @@ templates['leaderboardAnimal'] = template({"compiler":[8,">= 4.3.0"],"main":func
         return undefined
     };
 
-  return " <ul class=\"animal\">\r\n     <li class=\"animal-rank\">\r\n         <i class=\"fas fa-trophy\"></i> \r\n     </li>\r\n     <li class=\"animal-pic\">\r\n         <img class=\"animalImage\" src=\""
-    + alias4(((helper = (helper = lookupProperty(helpers,"animalImage") || (depth0 != null ? lookupProperty(depth0,"animalImage") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"animalImage","hash":{},"data":data,"loc":{"start":{"line":6,"column":39},"end":{"line":6,"column":54}}}) : helper)))
-    + "\">\r\n     </li>\r\n     <li class=\"animal-name\">\r\n         "
-    + alias4(((helper = (helper = lookupProperty(helpers,"animalName") || (depth0 != null ? lookupProperty(depth0,"animalName") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"animalName","hash":{},"data":data,"loc":{"start":{"line":9,"column":9},"end":{"line":9,"column":23}}}) : helper)))
-    + "\r\n     <li class=\"animal-type\">\r\n         "
-    + alias4(((helper = (helper = lookupProperty(helpers,"animalType") || (depth0 != null ? lookupProperty(depth0,"animalType") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"animalType","hash":{},"data":data,"loc":{"start":{"line":11,"column":9},"end":{"line":11,"column":23}}}) : helper)))
-    + "\r\n     </li>\r\n     <li class=\"animal-age\">\r\n         "
-    + alias4(((helper = (helper = lookupProperty(helpers,"animalAge") || (depth0 != null ? lookupProperty(depth0,"animalAge") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"animalAge","hash":{},"data":data,"loc":{"start":{"line":14,"column":9},"end":{"line":14,"column":22}}}) : helper)))
-    + "\r\n     </li>\r\n     </li>\r\n </ul>";
+  return "<ul class=\"animal\">\n    <div class=\"animal-rank-trophy\">\n        <i class=\"fas fa-trophy\"></i>\n    </div>\n    <div class=\"animal-rank\">15</div>\n    <h3 class=\"animal-pic\">\n        <img class=\"animalImage\" src=\""
+    + alias4(((helper = (helper = lookupProperty(helpers,"animalImage") || (depth0 != null ? lookupProperty(depth0,"animalImage") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"animalImage","hash":{},"data":data,"loc":{"start":{"line":7,"column":38},"end":{"line":7,"column":53}}}) : helper)))
+    + "\">\n    </h3>\n    <h3 class=\"animal-name\">\n        "
+    + alias4(((helper = (helper = lookupProperty(helpers,"animalName") || (depth0 != null ? lookupProperty(depth0,"animalName") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"animalName","hash":{},"data":data,"loc":{"start":{"line":10,"column":8},"end":{"line":10,"column":22}}}) : helper)))
+    + "\n    <h5 class=\"animal-type\">\n        Type: "
+    + alias4(((helper = (helper = lookupProperty(helpers,"animalType") || (depth0 != null ? lookupProperty(depth0,"animalType") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"animalType","hash":{},"data":data,"loc":{"start":{"line":12,"column":14},"end":{"line":12,"column":28}}}) : helper)))
+    + "\n    </h5>\n    <h5 class=\"animal-age\">\n        Age: "
+    + alias4(((helper = (helper = lookupProperty(helpers,"animalAge") || (depth0 != null ? lookupProperty(depth0,"animalAge") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"animalAge","hash":{},"data":data,"loc":{"start":{"line":15,"column":13},"end":{"line":15,"column":26}}}) : helper)))
+    + "\n    </h5>\n    <h5 class=\"animal-votes\">\n        Votes: 5\n    </h5>\n</ul>\n\n \n";
 },"useData":true});
 })();
