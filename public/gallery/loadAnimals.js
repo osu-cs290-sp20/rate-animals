16251b3743ffa0fe31c7203aa347f9fae2abf503
Malini <malini@Malinis-MacBook-Pro.local>
2020-06-11 21:38:32 -0700

    changes

--- public/gallery/loadAnimals.js
+++ public/gallery/loadAnimals.js
@@ -82,5 +82,31 @@ function demo(){
         box.removeChild(box.firstChild);
     }
     
-
 }
+
+
+var searchButton = document.getElementById("navbar-search-button");
+
+searchButton.addEventListener('click',  function (event){
+    console.log("the search button was clicked");
+    var searchInput = document.getElementById("navbar-search-input").value.toLowerCase();
+    console.log(searchInput);
+
+    var galleryBoxes = document.getElementsByClassName("gallery");
+
+    for(var i=0; i < galleryBoxes.length; i++){
+        var animalTitle = document.getElementsByClassName("animalName")[i].textContent.toLowerCase();
+        if (animalTitle.indexOf(searchInput) === -1){
+            galleryBoxes[i].style.visibility = "hidden";
+        }
+      }
+
+    if (searchInput === ""){
+        for (var i=0; i < galleryBoxes.length; i++){
+            galleryBoxes[i].style.visibility = "visible";
+        }
+      }
+
+})
+
+var searchButton = document.getElementById("navbar-search-button");
